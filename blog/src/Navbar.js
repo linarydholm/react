5c574ef837130a0cb7fadb55bbba1718baffdb19
Lina Rydholm <linarydholm@Linas-MacBook-Pro.local>
2023-08-18 14:01:20 +0200

    lesson 20

--- blog/src/Navbar.js
+++ blog/src/Navbar.js
@@ -1,13 +1,17 @@
 const Navbar = () => {
-    return ( 
-        <nav className="navbar">
-            <h1>React blog</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New blogpost</a>
-            </div>
-        </nav>
-     );
-}
- 
-export default Navbar;
+    return (
+      <nav className="navbar">
+        <h1>The Dojo Blog</h1>
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/create" style={{ 
+            color: 'white', 
+            backgroundColor: '#f1356d',
+            borderRadius: '8px' 
+          }}>New Blogpost</a>
+        </div>
+      </nav>
+    );
+  }
+   
+  export default Navbar;
